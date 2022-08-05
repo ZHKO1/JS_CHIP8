a8f6710a1a75591e1b8bf43e7040c4996ff97c5a
ZHKO1 <zhko11993@gmail.com>
2022-08-05 21:11:53 +0800

    rename SCREENSHOTS_GOLDEN

--- tests/SnapShots/config.js
+++ tests/SnapShots/config.js
@@ -1,4 +1,4 @@
-const SCREENSHOTS_GOLDEN = __dirname + "/SCREENSHOTS_GOLDEN"
+const SCREENSHOTS_GOLDEN = __dirname + "/SCREENSHOTS_GODHAND"
 const SCREENSHOTS = __dirname + "/SCREENSHOTS"
 const COMPAREDIFF = __dirname + "/COMPAREDIFF"
 const ROMARRAY = ["bc_test.ch8", "c8_test.c8", "test_opcode.ch8"];
